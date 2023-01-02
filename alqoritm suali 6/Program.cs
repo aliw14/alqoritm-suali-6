f1bf26d2ba7425ca028a6777c9a7ffab8cf40c15
ali <ali@MacBook-Pro-Ali.local>
2023-01-03 02:15:07 +0400

    mesaj

---
+++ alqoritm suali 6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace alqoritm_suali_6;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int count = 0;
+        int sum = 0;
+        int[] numbers = { 2, 3, 4, 5, 6, 7 };
+        for (int i = 1; i < numbers.Length; i += 2)
+
+        {
+
+            if (numbers[i] % 2 == 1)
+            {
+                count++;
+                sum += numbers[i];
+            }
+
+        }
+        Console.WriteLine(sum + " " + count);
+    }
+}
+
